54423cf645c11c4b7e0748c25cbf02a0c2a446e5
Naren-HappySanz <naren@happysanztech.com>
2019-08-16 15:37:57 +0530

    New Test

---
+++ app/src/main/java/com/skilex/customer/activity/AboutUsActivity.java
@@ -0,0 +1,40 @@
+package com.skilex.customer.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.skilex.customer.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    private static final String TAG = AboutUsActivity.class.getName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        findViewById(R.id.back_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        WebView wb = (WebView) findViewById(R.id.webView1);
+
+
+            wb.loadUrl("https://www.skilex.in");
+            wb.setWebViewClient(new WebViewClient(){
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url){
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
+    }
+
+}
