bf8b7b13069540466782b90a1478aaa201d7dab3
Naren <naren@happysanztech.com>
2020-09-07 16:22:40 +0530

    Testing bug fix

--- app/src/main/java/com/skilex/customer/activity/AddressActivity.java
+++ app/src/main/java/com/skilex/customer/activity/AddressActivity.java
@@ -613,25 +613,24 @@ public class AddressActivity extends FragmentActivity implements GoogleApiClient
 
     }
 
-    private double distance(double lat1, double lon1, double lat2, double lon2) {
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1))
-                * Math.sin(deg2rad(lat2))
-                + Math.cos(deg2rad(lat1))
-                * Math.cos(deg2rad(lat2))
-                * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
-        return (dist);
-    }
+    private double distance(double lat1, double lng1, double lat2, double lng2) {
 
-    private double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-    }
+        double earthRadius = 6371; // in miles, change to 6371 for kilometer output
 
-    private double rad2deg(double rad) {
-        return (rad * 180.0 / Math.PI);
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+
+        double sindLat = Math.sin(dLat / 2);
+        double sindLng = Math.sin(dLng / 2);
+
+        double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        double dist = earthRadius * c;
+
+        return dist; // output distance, in MILES
     }
 
     private void sendVals(String id, String latLng, String newDate) {
